fdf43bf1f8a56cdc39c9d5ebeaa709a8ae2e11d9
Shoyas <md.nasiruddin4067@gmail.com>
2021-04-14 18:19:50 +0600

    card design running

--- src/Component/BodyPage/Cards/Cards.jsx
+++ src/Component/BodyPage/Cards/Cards.jsx
@@ -8,10 +8,10 @@ const Cards = ({product}) => {
     console.log("Product: ", product);
     console.log("Stocks: ", stocks);
 
-    let colorsEven = [];
-    let colorsOdd = [];
-    console.log("colorsEven :", colorsEven);
-    console.log("colorsOdd :", colorsOdd);
+    // let colorsEven = [];
+    // let colorsOdd = [];
+    // console.log("colorsEven :", colorsEven);
+    // console.log("colorsOdd :", colorsOdd);
 
     // for(let i=0; i< stocks.length; i++){
     //     let id = stocks[i].id;
@@ -33,18 +33,35 @@ const Cards = ({product}) => {
 
     // }
 
+
+    // let colorsEven = ["textRed"];
+    let card1 = ["card-1"];
+    let card2 = ["card-2"];
+    let card3 = ["card-3"];
+    let card4 = ["card-4"];
+    let demo = ["demo"];
+
+    let sideBarLeft = ["card-body-1"];   // upper and left
+    // let sideBarRight = ["card-container"]; // right and bottom
+
+    let sideBarRight = ["card-container-1"]; // right and bottom
+
+
+    // console.log("colorsEven :", colorsEven);
+    // console.log("colorsOdd :", colorsOdd);
+
     console.log(stocks.length);
 
-    if( stocks.length % 2 === 0){
-        if(colorsEven.length === 0){
-            colorsEven.push("textRed");
-        }
-    }
-    if( stocks.length % 2 === 1){
-        if(colorsOdd.length < 0){
-            colorsOdd.push("textGreen");
-        }
-    }
+    // if( stocks.length % 2 === 0){
+    //     if(colorsEven.length === 0){
+    //         colorsEven.push("textRed");
+    //     }
+    // }
+    // if( stocks.length % 2 === 1){
+    //     if(colorsOdd.length < 0){
+    //         colorsOdd.push("textGreen");
+    //     }
+    // }
 
     
     
@@ -57,12 +74,29 @@ const Cards = ({product}) => {
                     stocks.map((stocks) => { 
                         return(
                             <div className="col-md-3" >
-                                <div className="card-container">
+                                <div className={`${
+                                    // card-container
+                                    stocks.Product.id === 4 ? `${sideBarRight}`: `${demo}` 
+
+                                }`}>
                                     <span className="card-header"></span>
-                                    <div className={`card-body ${
-                                        stocks.Product.id % 2 === 1 ? `${colorsOdd}`: `${colorsEven}` 
+                                    <div className={`${
+                                        // card-body
+                                        stocks.Product.id === 1 ? `${sideBarLeft}`: `${demo}` 
+                                        // &&
+                                        // stocks.Product.id === 4 ? `${sideBarRight}`: `${demo}` 
+
+                                    } ${
+                                        stocks.Product.id === 1 ? `${card1}`: `${demo}` 
                                         &&
-                                        stocks.Product.id % 2 === 0 ? `${colorsEven}` : `${colorsOdd}` 
+                                        stocks.Product.id === 2 ? `${card2}`: `${demo}`
+                                        &&
+                                        stocks.Product.id === 3 ? `${card3}`: `${demo}`
+                                        &&
+                                        stocks.Product.id === 4 ? `${card4}`: `${demo}`
+
+                                        // &&
+                                        // stocks.Product.id % 2 === 0 ? `${colorsEven}` : `${colorsOdd}` 
                                     }`}>
 
                                         <div className="card-image img-fluid">
@@ -71,6 +105,9 @@ const Cards = ({product}) => {
                                         <div className="card-tag text-center">
                                             <small><strong>{stocks.Product.name}</strong></small>
                                         </div>
+                                    </div>
+                                    <div className="card-footer">
+
                                     </div>
                                 </div>
                             </div>
